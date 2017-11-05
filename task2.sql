5f54fef85236854aa997e0a0328eea169ca0fdde
Eugene Khorev <eugene.khorev@gmail.com>
2017-11-05 15:47:04 +0300

    - Added SQL task script

---
+++ task2.sql
@@ -0,0 +1,32 @@
+/**
+ * Author:  eugene
+ * Created: Nov 5, 2017
+ */
+
+DROP TABLE IF EXISTS users_sql;
+
+CREATE TABLE users_sql (
+	id bigserial, 
+	group_id bigint
+);
+
+
+INSERT INTO users_sql (group_id) 
+	VALUES (1), (1), (1), (2), (1), (3);;
+	
+--INSERT INTO users_sql (group_id) 
+--	VALUES (1), (1), (1), (2), (1), (1), (3), (2), (2), (3);
+
+SELECT 
+	group_id,
+	MIN(id) AS start_id,
+	COUNT(*) AS num
+FROM (
+	SELECT 
+		id,
+		group_id,
+		ROW_NUMBER() OVER (ORDER BY id) - ROW_NUMBER() OVER (PARTITION BY group_id ORDER BY id) AS num
+	FROM users_sql
+) AS result 
+GROUP BY group_id, num
+ORDER BY start_id;
